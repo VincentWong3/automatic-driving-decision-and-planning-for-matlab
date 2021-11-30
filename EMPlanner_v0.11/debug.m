4a3a63eeadc386acf4ccbed0a1296d22e34e77e2
VincentWong3 <72872693+VincentWong3@users.noreply.github.com>
2021-11-30 23:19:51 +0800

    Add files via upload
    第二章第二节(下）代码，已修复已知bug 可能有未知bug

---
+++ EMPlanner_v0.11/debug.m
@@ -0,0 +1,3 @@
+%plot(debug_planning.debug_planning_info.xr_desire_m_.data,debug_planning.debug_planning_info.yr_desire_m_.data)
+%plot(debug_planning.debug_planning_info.xr_desire_m_.time,debug_planning.debug_planning_info.xr_desire_m_.data)
+plot(debug_planning.debug_planning_info.xr_desire_m_.time,debug_planning.debug_planning_info.thetar_desire_rad_.data)
