37a29d5a581a4f9df56da0062268260ca92f24bc
VincentWong3 <72872693+VincentWong3@users.noreply.github.com>
2022-08-07 00:42:56 +0800

    增加速度规划，避让简单动态障碍物

---
+++ EMPlannerV0.5/debug.m
@@ -0,0 +1,3 @@
+%plot(debug_planning.debug_planning_info.xr_desire_m_.data,debug_planning.debug_planning_info.yr_desire_m_.data)
+%plot(debug_planning.debug_planning_info.xr_desire_m_.time,debug_planning.debug_planning_info.xr_desire_m_.data)
+plot(debug_control.xr_desire.data,debug_control.yr_desire.data)
